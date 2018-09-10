77c0e9fdf24b27210898a71ea46d14e9151ee711
Artur Morozov <Artur91425@gmail.com>
2018-09-10 16:02:07 +0300

    1.0-r27740

---
+++ Mendeleev/locales/MendeleevGlobals.esES.lua
@@ -0,0 +1,165 @@
+local L = AceLibrary("AceLocale-2.2"):new("Mendeleev")
+
+L:RegisterTranslations("esES", function() return {
+	["KCI Integration"] = "Integraci\195\179n con KCI",
+	["Integrate into the KCI tooltip"] = "Integrar en el tooltip de KCI",
+	["integrate"] = "integrar",
+	["sets"] = "conjuntos",
+	["toggle"] = "activar",
+	["Toggle Sets"] = "Activa conjuntos",
+
+	["Crafted by"] = "Creado por",
+	["Crafted by: "] = "Creado por: ",
+
+	["Recipe source"] = "Receta",
+	["Recipe source: "] = "Receta: ",
+	["Seasonal recipe"] = "Receta estacional",
+	["Recipe faction"] = "Facci\195\179n de la receta",
+	["Vendor faction"] = "Facci\195\179n de la receta",
+
+	["Gathering skills"] = "Habilidad",
+	["Mine Gems"] = "Gemas de mina",
+	["Trade Skills"] = "Profesiones",
+	["Class Reagents"] = "Componentes de clase",
+	["Zul'Gurub Enchants"] = "Encantamientos de Zul'Gurub",
+	["Zul'Gurub Classes"] = "Clases de Zul'Gurub",
+	["Zul'Gurub Coins"] = "Monedas de Zul'Gurub",
+	["Ahn'Qiraj CC Classes"] = "Clases CC de Ahn'Qiraj",
+	["Ahn'Qiraj BoN Classes"] = "Clases BoN de Ahn'Qiraj",
+	["Food type"] = "Tipo de comida",
+	["Booze"] = "Alcohol",
+	["Lockpick value"] = "Valor de forzar cerradura",
+	["World Drops"] = "Bot\195\173n de mundo",
+	["World Boss Drops"] = "Bot\195\173n de jefes de mundo",
+	["Raid Zones"] = "Zonas de banda",
+	["Raid Bosses"] = "Jefes de banda",
+	["Instance zones"] = "Zonas de instancia",
+	["Instance bosses"] = "Jefes de instancia",
+	["Elemental bosses"] = "Jefes elementales",
+	["Outdoor bosses"] = "Jefes a campo abierto",
+	["Four Dragons"] = "Cuatro Dragones",
+
+	["Gathered by"] = "Recogido por",
+	["Zul'Gurub Enchant"] = "Encantamiento de Zul'Gurub",
+	["Used by"] = "Usado por",
+	["Classes"] = "Clases",
+	["Darkmoon Faire"] = "Feria de la Luna Negra",
+	["Darkmoon Faire Card"] = "Carta de Feria de la Luna Negra",
+	["World Drop"] = "Bot\195\173n de mundo",
+	["World Boss Drop"] = "Bot\195\173n de jefe de mundo",
+	["Found in"] = "Encontrado en",
+	["Dropped by"] = "Soltado por",
+	["Used in"] = "Usado en",
+	["Reputaton item"] = "Objetos de reputaci\195\179n",
+
+	["Copper"]        = "Cobre",
+	["Tin"]           = "Esta\195\177o",
+	["Silver"]        = "Plata",
+	["Iron"]          = "Hierro",
+	["Gold"]          = "Oro",
+	["Mithril"]       = "Mitril",
+	["Truesilver"]    = "Veraplata",
+	["Small Thorium"] = "Torio peque\195\177o",
+	["Hakkari Thorium"]                 = "Torio Hakkari",
+	["Rich Thorium"]  = "Torio enriquecido",
+	["Dark Iron"]     = "Hierro negro",
+
+	["Fishing"]       = "Pesca",
+	["Disenchant"]    = "Desencantar",
+	["Herbalism"]     = "Herborister\195\173a",
+	["Mining"]        = "Miner\195\173a",
+	["Skinning"]      = "Desuello",
+	["Alchemy"]       = "Alquimia",
+	["Blacksmithing"] = "Herrer\195\173a",
+	["Cooking"]       = "Cocina",
+	["Enchanting"]    = "Encantamiento",
+	["Engineering"]   = "Ingenier\195\173a",
+	["Firstaid"]      = "Primeros auxilios",
+	["Leatherworking"]= "Peleter\195\173a",
+	["Tailoring"]     = "Sastrer\195\173a",
+
+	["Fish"]          = "Pescado",
+	["Meat"]          = "Carne",
+	["Bread"]         = "Pan",
+	["Conjured"]      = "Conjurado",
+	["Cheese"]        = "Queso",
+	["Fruit"]         = "Fruta",
+	["Misc"]          = "Misc",
+	["Fungus"]        = "Hongo",
+
+	["Junk Items"]    = "Objetos basura",
+	["Leather"]       = "Cuero",
+	["Blue Dragon Card"]                = "Carta de Drag\195\179n Azul",
+	["Heroism Card"]  = "Carta de Heroismo",
+	["Twisting Nether Card"]            = "Carta de Vac\195\173o abisal",
+	["Maelstrom Card"]= "Carta de La Vor\195\161gine",
+
+	["Lockpicking"]   = "Ganz\195\186as",
+	[" (Enchant)"]    = " (Encantamiento)",
+	["Trash Mobs"]    = "Enemigos basura",
+	["Item ID"]        = "ID de objeto",
+	["Stacksize"]     = "Tama\195\177o de lote",
+	["NoClue"]        = "No s\195\169 ",
+
+	-- Factions
+	["Alliance"] = "Alianza",
+	["Horde"] = "Horda",
+	["Neutral"] = "Neutral",
+	["Argent Dawn"] = "Alba Argenta",
+	["Cenarion Circle"] = "C\195\173rculo Cenarion",
+	["Thorium Brotherhood"] = "Hermandad del torio",
+	["Timbermaw Hold"] = "Basti\195\179n Fauces de Madera",
+	["Zandalar Tribe"] = "Tribu Zandalar",
+	["Ravenholdt"] = "Ravenholdt",
+
+	-- Crafted by
+	["Alchemy"] = "Alquimia",
+	["Blacksmithing"] = "Herrer\195\173a",
+	["Armorsmithing"] = "Forjador de armaduras",
+	["Axesmithing"] = "Forjador de hachas",
+	["Hammersmithing"] = "Forjador de martillos",
+	["Swordsmithing"] = "Forjador de espadas",
+	["Weaponsmithing"] = "Forjador de armas",
+	["Cooking"] = "Cocina",
+	["Engineering"] = "Ingenier\195\173a",
+	["Gnomish Engineering"] = "Ingenier\195\173a gnoma",
+	["Goblin Engineering"] = "Ingenier\195\173a goblin",
+	["First Aid"] = "Primeros auxilios",
+	["Leatherworking"] = "Peleter\195\173a",
+	["Dragonscale Leatherworking"] = "Peleter\195\173a dragontina",
+	["Elemental Leatherworking"] = "Peleter\195\173a elemental",
+	["Tribal Leatherworking"] = "Peleter\195\173a tribal",
+	["Tailoring"] = "Sastrer\195\173a",
+
+	-- Sources
+	["Drop"] = "Bot\195\173n",
+	["Vendor"] = "Vendedor",
+	["Quest"] = "Misi\195\179n",
+	["Crafted"] = "Creado",
+
+	["UBRS"] = "CdRN",
+	["LBRS"] = "PdRN",
+}end)
+
+if GetLocale() == "esES" then
+
+--Localized strings
+--This has a slightly different structure because of the way the closure in the cats works
+MendeleevLocals.Rep = {}
+MendeleevLocals.Rep[1] = " (Odiado)"
+MendeleevLocals.Rep[2] = " (Hostil)"
+MendeleevLocals.Rep[3] = " (Enemigo)"
+MendeleevLocals.Rep[4] = " (Neutral)"
+MendeleevLocals.Rep[5] = " (Amistoso)"
+MendeleevLocals.Rep[6] = " (Honorable)"
+MendeleevLocals.Rep[7] = " (Reverenciado)"
+MendeleevLocals.Rep[8] = " (Exaltado)"
+
+MendeleevLocals.Dif = {}
+MendeleevLocals.Dif[0] = "(Trivial)"
+MendeleevLocals.Dif[1] = "(F\195\161cil)"
+MendeleevLocals.Dif[2] = "(Medio)"
+MendeleevLocals.Dif[3] = "(\195\147ptimo)"
+MendeleevLocals.Dif[4] = "(Dif\195\173cil)"
+
+end
